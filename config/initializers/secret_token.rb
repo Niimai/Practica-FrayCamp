fb147eeb40f15758ac1f3fb3a7aba4c44cae7a2d
Skywalker <skywalker@skywalker-MacBook.(none)>
2013-02-17 20:22:19 +0000

    mi primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fraycamp::Application.config.secret_token = 'f7a446bfd28aabcd4fff0093452e324a438c385a661305866b351dc099839431bd2e4bfd149e7cbc68ba816dc218821efa7e0152dbd1bb43065b7b5dfca698f1'
